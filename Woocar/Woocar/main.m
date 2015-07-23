8adab6be87453239e2189553f341cd0432fe0e5f
khairulalamruet <khairulalamruet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-23 09:46:28 +0000

    New Import
    git-svn-id: https://plugins.svn.wordpress.org/twi-woocommerce-gridslidercarousel-lite/trunk@1204679 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Woocar/Woocar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Woocar
+//
+//  Created by Khairul Alam on 7/23/15.
+//  Copyright (c) 2015 Khairul Alam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
