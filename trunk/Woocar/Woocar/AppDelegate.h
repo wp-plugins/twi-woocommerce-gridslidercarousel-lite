680fc8c10e1559396bb6eab168283b35674be53c
khairulalamruet <khairulalamruet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-23 09:46:28 +0000

    New Import
    git-svn-id: https://plugins.svn.wordpress.org/twi-woocommerce-gridslidercarousel-lite@1204679 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/Woocar/Woocar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Woocar
+//
+//  Created by Khairul Alam on 7/23/15.
+//  Copyright (c) 2015 Khairul Alam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ trunk/Woocar/Woocar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Woocar
+//
+//  Created by Khairul Alam on 7/23/15.
+//  Copyright (c) 2015 Khairul Alam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
